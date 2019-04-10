76934ae039e9fd1aea0de9badc9f73dc1e9544a9
Gustavo da Silva Machado <48689692+Guga15gu@users.noreply.github.com>
2019-04-10 11:06:21 -0300

    Update Agenda.c

--- Agenda.c
+++ Agenda.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void *pBuffer;
+int *i,*s;
+
 struct pessoa{
 
     char nome[20];
@@ -9,101 +12,100 @@ struct pessoa{
 
 } pessoa;
 
-void realocar(int *pBuffer, int *i, int *s){
-	
+void realocar(void){
+
     pBuffer = realloc( pBuffer, *i * sizeof(int) + *s * sizeof(struct pessoa));
 
     i = pBuffer;
     s = i + 1;
-    
+
 }
 
-void corrigir(int *pBuffer, int *i, int *s){
-	
+void corrigir(void){
+
 //	int *z = pBuffer;
-	
+
 //	z = z + sizeof(int) * *i;
-	
+
 //	memmove()
-	
-	
-	
-	
+
+
+
+
 }
-	
-void adicionar(int *pBuffer, int *i, int *s){
+
+void adicionar(void){
 
     struct pessoa *pessoa1;
  	++*s;
-    realocar(pBuffer, i, s);
+    realocar();
 
     i = pBuffer;
     s = i + 1;
-    pessoa1 =  pBuffer + (*i)* sizeof(int) + sizeof(struct pessoa) * (*s - 1);
+    pessoa1 =  pBuffer + (*i)* sizeof(int) + sizeof(struct pessoa) * (*s-1);
 
     printf("Digite o nome desejado: \n");
     scanf("%s", pessoa1->nome);
-    	
+
     printf("Digite a idade de %s: \n", pessoa1->nome);
     scanf("%d", &pessoa1->idade);
-    
-    
+
+
     printf("Digite o número de telefone de %s: \n", pessoa1->nome);
     scanf("%d", &pessoa1->telefone);
-    
-    
+
+
 }
 
-void listar(int *pBuffer, int *i, int *s){
-	
+void listar(void){
+
 	int* count;
 	count = pBuffer + sizeof(int) * 3 ;
-	
+
 	struct pessoa * campo;
 	campo = pBuffer + sizeof(int) * *i;
-	
+
 	for(*count = 0; *count< *s; ++*count){
 		printf("\n");
 		printf( "Nome: %s\n", campo->nome );
 		printf("Idade: %d\n", campo->idade );
 		printf("Telefone: %d\n\n\n", campo->telefone );
 		campo = campo + sizeof(struct pessoa);
-	
-	
+
+
 	}
-  
+
 }
 
 int main()
 {
-  void *pBuffer;
-  int *i, *s, *menu;
+  int *menu;
 
   pBuffer = malloc(sizeof(4 * sizeof(int)));
   i = pBuffer;
   s = i + 1;
   *i = 4;
   *s = 0;
-  
+
   menu = pBuffer + 2 * sizeof(int);
-  
+
   printf("\nVocê deseja: \n 1: Adicionar uma pessoa.\n 2: Excluir uma pessoa.\n 3: Buscar uma pessoa.\n 4: Listar.\n 5: sair\n ");
   scanf("%d", menu);
 
   while(*menu != 5){
-	  
+
 	switch (*menu){
-	
+
 		case 1:
-	
-			adicionar(pBuffer, i, s);
+
+			adicionar();
 
 			break;
 
 		case 2:
-		
+
 			//apagar(pBuffer, i, s);
-		
+
 			break;
 
 		case 3:
@@ -111,10 +113,10 @@ int main()
 			break;
 
 		case 4:
-			
-			listar(pBuffer, i, s);
+
+			listar();
 			break;
-		
+
 
 	}
 	printf("Você deseja: \n 1: Adicionar uma pessoa.\n 2: Excluir uma pessoa.\n 3: Buscar uma pessoa.\n 4: Listar.\n 5: sair\n ");
