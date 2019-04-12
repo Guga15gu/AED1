35e1fce1d55c5395e985a87cd92d94d080d249ca
Gustavo da Silva Machado <48689692+Guga15gu@users.noreply.github.com>
2019-04-12 03:28:08 -0300

    Update Agenda.c
    Agenda quase pronta(se considerar que o pBuffer terá uma quantidade de auxiliares fixa - int's,(algum dia crio uma função que move ou copie a memória, consequentemente deixando apenas os auxiliares em uso mallocados), só falta descobrir um jeito de buscar por nome( talvez  alocar x inteiros, pra poder armazenar um char[20]), além, é claro, de arrumar os espaçamentos e as mensagens dos printfs.

--- Agenda.c
+++ Agenda.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void *pBuffer;
 int *i,*s;
@@ -33,10 +34,6 @@ void corrigir(void){
 //	z = z + sizeof(int) * *i;
 
 //	memmove()
-
-
-
-
 }
 
 void adicionar(void){
@@ -55,11 +52,123 @@ void adicionar(void){
     printf("Digite a idade de %s: \n", pessoa1->nome);
     scanf("%d", &pessoa1->idade);
 
-
     printf("Digite o número de telefone de %s: \n", pessoa1->nome);
     scanf("%d", &pessoa1->telefone);
 
+}
 
+void* buscar(){
+
+    int* count;
+	count = (void*)pBuffer + sizeof(int) * 3 ;
+
+    struct pessoa * campo;
+	campo = (void *)pBuffer + sizeof(int) * *i;
+
+    int *a;
+    a = (void*)pBuffer + sizeof(int) * 4 ;
+
+    void *item;
+    item = (void*)pBuffer + sizeof(int) * 5;
+
+//Só falta modificar o código para buscar pelo nome...
+    printf("Por qual campo você deseja buscar? \n1 nome; \n2 idade; \n3 telefone.\n");
+    scanf("%d", a);
+    int * itemi;                //precisei fazer essas gambiarras pro compilador resolver cumprir sua função
+    char * itemc;               //precisei fazer essas gambiarras pro compilador resolver cumprir sua função*
+    if(*a==1){
+            itemc = item;       //precisei fazer essas gambiarras pro compilador resolver cumprir sua função**
+
+            printf("Digite o nome a ser buscado: \n");
+            scanf("%s", itemc); 
+    }
+    else if(*a==2){
+            itemi = item;       //precisei fazer essas gambiarras pro compilador resolver cumprir sua função***
+
+            printf("Digite a idade a ser buscada: \n");
+            scanf("%d", itemi); 
+    }
+    else {
+            itemi = item;       //precisei fazer essas gambiarras pro compilador resolver cumprir sua função****
+
+            printf("Digite o telefone a ser buscado: \n");
+            scanf("%d", itemi);
+    }
+
+    for(*count = 0; *count< *s; ++*count){
+
+	    if(*a==1){
+
+            if(itemc == campo->nome){
+
+                printf("\nNome: %s\n", campo->nome);
+		        printf("Idade: %d\n", campo->idade );
+	        	printf("Telefone: %d\n\n", campo->telefone );
+                return *count;
+            }
+        }
+
+        else if(*a==2){
+            
+            if(*itemi == campo->idade ){
+                
+                printf("\nNome: %s\n", campo->nome);
+		        printf("Idade: %d\n", campo->idade );
+	        	printf("Telefone: %d\n\n", campo->telefone );
+                return *count;
+            }
+        }
+        else {
+            
+            if(*itemi == campo->telefone){
+
+                printf("\nNome: %s\n", campo->nome);
+		        printf("Idade: %d\n", campo->idade );
+	        	printf("Telefone: %d\n\n", campo->telefone );
+                return *count;
+            }
+        }
+        
+		campo = (void *)campo + sizeof(struct pessoa);
+        
+	}
+    printf("\nContato inexistente.\n\n");
+	
+    return -1;
+}
+
+void apagar(void){
+
+    struct pessoa * campo;
+    int* count;
+	count = (void*)pBuffer + sizeof(int) * 3 ;
+
+    *count = buscar();
+    campo = (void *)pBuffer + sizeof(int) * *i + sizeof(struct pessoa) * *count;
+    
+    if(*count == -1){
+        printf("\nContato inexistente.\n\n");
+    }
+
+    else{
+
+        printf("\nExcluindo contato acima...\n");
+        *s = *s - 1;
+        struct pessoa * aux;
+        aux = campo + 1;
+        for(; *count< *s; ++*count){
+
+            strcpy(campo->nome, aux->nome);
+            campo->idade = aux->idade;
+            campo->telefone = aux->telefone;
+            
+		    campo = (void *)campo + sizeof(struct pessoa);
+            aux = (void *)aux + sizeof(struct pessoa);
+	    }
+        realocar();
+        printf("\n Sucesso na exclusão;\n");
+    }
+    
 }
 
 void listar(void){
@@ -74,19 +183,18 @@ void listar(void){
 		printf("\n");
 		printf( "Nome: %s\n", campo->nome );
 		printf("Idade: %d\n", campo->idade );
-		printf("Telefone: %d\n\n\n", campo->telefone );
+		printf("Telefone: %d\n", campo->telefone );
 		campo = (void *)campo + sizeof(struct pessoa);
 
-
 	}
-
+    printf("\n");
 }
 
 int main()
 {
   int *menu;
 
-  pBuffer = malloc(sizeof(4 * sizeof(int)));
+  pBuffer = malloc(sizeof(6 * sizeof(int)));
 
   if(pBuffer==NULL){
       printf("Erro na alocação.\n\n");
@@ -95,7 +203,7 @@ int main()
 
   i = pBuffer;
   s = i + 1;
-  *i = 4;
+  *i = 6;
   *s = 0;
 
   menu = (void*) pBuffer + 2 * sizeof(int);
@@ -110,17 +218,16 @@ int main()
 		case 1:
 
 			adicionar();
-
 			break;
 
 		case 2:
 
-			//apagar(pBuffer, i, s);
-
+			apagar();
 			break;
 
 		case 3:
 
+            buscar();
 			break;
 
 		case 4:
@@ -130,10 +237,15 @@ int main()
 
 
 	}
+
 	printf("Você deseja: \n 1: Adicionar uma pessoa.\n 2: Excluir uma pessoa.\n 3: Buscar uma pessoa.\n 4: Listar.\n 5: sair\n ");
 	menu = (void*) pBuffer +  2 * sizeof(int);
     scanf("%d", menu);
    }
 
+    if(*menu == 5){
+        free(pBuffer);
+        printf("\nFree executado.\n");
+    }
     return 0;
 }
